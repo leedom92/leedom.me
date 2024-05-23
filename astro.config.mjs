3ba38895900133efc57575c8cbf8337601570bcc
Leedom <jld1221@163.com>
2024-05-23 22:40:48 +0800

    fix: meta

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://leedom.me',
   integrations: [
     mdx(), 
     sitemap(), 
