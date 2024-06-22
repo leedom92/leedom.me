51a1c8f990c9478159d1d499d218d50ff0b9367f
Leedom <jld1221@163.com>
2024-06-22 13:41:45 +0800

    chore: update deps & astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,12 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 
-import tailwind from '@astrojs/tailwind'
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://leedom.me',
   integrations: [
     mdx(),
     sitemap(),
-    tailwind({
-      nesting: true,
-    }),
   ],
   markdown: {
     shikiConfig: {
