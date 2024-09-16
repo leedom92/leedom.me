01c4aca7e84ae17cb2cfc54c2652ad447d9a25f0
Leedom <jld1221@163.com>
2024-09-16 23:05:58 +0800

    feat: add vue component - ScrollToTop

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,12 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 
+import vue from '@astrojs/vue';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://leedom.me',
-  integrations: [
-    mdx(),
-    sitemap(),
-  ],
+  integrations: [mdx(), sitemap(), vue()],
   markdown: {
     shikiConfig: {
       // https://shiki.style/themes
---
+++ src/components/ScrollToTop.vue
@@ -0,0 +1,33 @@
+<template>
+  <button
+    title="Scroll to top"
+    aria-label="Scroll to top"
+    class="z-100 fixed bottom-3 right-3 h-10 w-10 rounded-full transition duration-300 hover:bg-[#8888] hover:opacity-100 dark:text-white dark:hover:bg-[#8883] print:hidden"
+    :class="scroll > 200 ? 'opacity-30' : 'pointer-events-none opacity-0'"
+    @click="toTop()"
+  >
+    <svg
+      class="m-auto w-5"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="currentColor"
+        d="M13 7.828V20h-2V7.828l-5.364 5.364l-1.414-1.414L12 4l7.778 7.778l-1.414 1.414z"
+      />
+    </svg>
+  </button>
+</template>
+
+<script setup>
+import { useWindowScroll } from '@vueuse/core'
+
+function toTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  })
+}
+
+const { y: scroll } = useWindowScroll()
+</script>
