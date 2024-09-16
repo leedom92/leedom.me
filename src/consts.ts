be8c969064f71c947cda955983fb8ec37e67cd79
Leedom <jld1221@163.com>
2024-09-16 16:31:29 +0800

    refactor: site description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Leedom'
-export const SITE_DESCRIPTION = "Leedom's homepage"
+export const SITE_DESCRIPTION = 'LEEDOM'
